7e6bfb0df9e1e05776ea3e9abe4651a01b936185
Carexo <marcel.mietla3@gmail.com>
2020-10-15 19:41:28 +0200

    Dodanie zdjec woj, dodanie przycisku powrotu

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
 function name(name) {
   document.getElementById("wojewodztwo").innerHTML = name;
   }
+
+document.onscroll=function() {
+  var ile=window.pageYOffset;
+	if(ile>=document.getElementById("body").offsetTop)
+		{
+		document.getElementById("header").style.position='fixed';
+		document.getElementById("poland_map").style.display="block";
+		}
+	else
+		{
+		document.getElementById("header").style.position='relative';
+		document.getElementById("poland_map").style.display="none";
+		}
+  }
+	document.getElementById('dogory').onclick=function()
+	{
+		window.scrollTo(0,0);
+  }
+  
+  $(document).ready(function(){ 
+
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 500) {
+            $('#dogory').fadeIn();
+        } else {
+            $('#dogory').fadeOut();
+        }
+    }); 
+
+    $('dogory').click(function(){
+        $("html, body").animate({ scrollTop: 0 }, 600);
+        return false;
+    });
+
+})
