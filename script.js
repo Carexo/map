f1f546c2c3ae66556091a1eaada8313dd1e76875
Carexo <marcel.mietla3@gmail.com>
2020-10-15 17:47:34 +0200

    javascript zrobienie

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
-}
-
-    function name(name) {
-        document.getElementById("wojewodztwo").innerHTML = name;
-    }
+function name(name) {
+  document.getElementById("wojewodztwo").innerHTML = name;
+  }
